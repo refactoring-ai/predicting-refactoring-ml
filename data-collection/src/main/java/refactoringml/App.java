9f82e9ec6567d04d7237e4a5c1d32b1e14cd0e6d
Mauricio Aniche <mauricioaniche@gmail.com>
2019-04-25 12:09:27 +0200

    if args is null, then it is a local test

--- data-collection/src/main/java/refactoringml/App.java
+++ data-collection/src/main/java/refactoringml/App.java
@@ -53,7 +53,7 @@ public class App {
 
 		// do we want to get data from the vars or not?
 		// i.e., is this a local IDE test?
-		boolean test = true;
+		boolean test = (args == null || args.length == 0);
 
 		String gitUrl;
 		String highLevelOutputPath;
