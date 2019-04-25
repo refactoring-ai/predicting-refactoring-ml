0790c876476e7cf0fc2502f87dac99a68adcac13
Vinicius Durelli <vinicius.durelli@gmail.com>
2019-04-25 09:08:46 -0300

    First go at implementing decision tree. I think there's room for improvement, butit's something already.

---
+++ traditional-ml/refactoring_models_decision_tree.py
@@ -0,0 +1,62 @@
+import db
+import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+
+
+#THIS IS DUPLICATED CODE (THE SAME CODE APPEARS IN THE REFACTORING_MODELS_SVM.PY FILE)
+#TODO: later on we should probably move this method to a separate file
+def perform_under_sampling(x, y):
+    rus = RandomUnderSampler(random_state=42) # 42 is a random number, just to ensure our results are reproducible
+    return rus.fit_resample(x, y)
+
+def run_decision_tree(m_refactoring, refactorings, non_refactored_methods, f):
+    assert refactorings.shape[0] > 0, "No refactorings found"
+    #set the prediction variable as true and false in the datasets
+    refactorings["prediction"] = 1
+    non_refactored_methods["prediction"] = 0
+
+    #combine both datasets (with both TRUE and FALSE predictions)
+    assert non_refactored_methods.shape[1] == refactorings.shape[1], "Datasets have a different number of columns"
+    merged_dataset = pd.concat([refactorings, non_refactored_methods])
+
+    #separate x from y 
+    x = merged_dataset.drop("prediction", axis=1)
+    y = merged_dataset["prediction"]
+
+    #balance the datasets 
+    #as mentioned elsewhere in the code, we have way more 'non-refactored examples' than refactoring examples
+    balanced_x, balanced_y = perform_under_sampling(x, y)
+    assert balanced_x.shape[0] == balanced_y.shape[0], "Undersampling did not work while building the decision tree model"
+
+    #perform some scaling to speed up the whole model-building thingy
+    scaler = MinMaxScaler()  #default behavior scales data to the following range: (0,1)
+    balanced_x = scaler.fit_transform(balanced_x)
+
+    #create [decision tree] model
+    print("Starting to build the decision tree for %s" % m_refactoring)
+    model = DecisionTreeClassifier(random_state=42)
+    model.fit(balanced_x, balanced_y)
+    
+    #perform 10-fold validation 
+    scores = cross_val_score(model, balanced_x, balanced_y, cv=10)
+    print(scores)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    
+    #output results to file 
+    f.write("\n---\n")
+    f.write(m_refactoring + "\n")
+    f.write("Instances: %d\n" % refactorings.shape[0])
+    f.write("Accuracy: %0.2f (+/- %0.2f)\n" % (scores.mean(), scores.std() * 2))
+    f.write("\nFeatures:\n")
+    f.write(', '.join(str(e) for e in list(x.columns.values)))
+    f.write("\n---\n")
+
+    return model
+
+
+run_decision_tree
+
