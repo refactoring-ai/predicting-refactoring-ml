178be1d6e832147c047b196cbda39b94e163c4be
Mauricio Aniche <mauricioaniche@gmail.com>
2019-05-02 09:50:22 +0200

    printing status of the execution in a file; copying log to the zip file; no need for the 8th parameter

--- data-collection/run-cloud.sh
+++ data-collection/run-cloud.sh
@@ -2,15 +2,15 @@
 
 export IFS=","
 
-if [ "$#" -ne 8 ]; then
+if [ "$#" -ne 7 ]; then
   echo "wrong usage" >&2
   exit 1
 fi
 
 CLASS="refactoringml.App"
-JAR_PATH=$8/predicting-refactoring-ml/data-collection/target/refactoring-analyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-REFACTORINGMINER_JAR_PATH=$8/predicting-refactoring-ml/data-collection/lib/RefactoringMiner-20190430.jar
-OUTPUT_PATH=$8/output
+JAR_PATH=predicting-refactoring-ml/data-collection/target/refactoring-analyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+REFACTORINGMINER_JAR_PATH=predicting-refactoring-ml/data-collection/lib/RefactoringMiner-20190430.jar
+OUTPUT_PATH=output
 PROJECTS_CSV_PATH=$1
 BEGIN=$2
 END=$3
@@ -24,12 +24,13 @@ mkdir $OUTPUT_PATH
 
 echo ""
 i=0
-cat $PROJECTS_CSV_PATH | while 
+cat $PROJECTS_CSV_PATH | while local
 	read PROJECT REPO DATASET; do
 	let "i++"
 
 	if [ $i -ge $BEGIN -a $i -le $END ]; then
-		echo "INIT $PROJECT"
+		echo "INIT $i = $PROJECT"
+		echo "$i=$PROJECT" >> execution.txt
 
 		OUTPUT_PROJECT_PATH="$OUTPUT_PATH/$PROJECT"
 		mkdir $OUTPUT_PROJECT_PATH
@@ -39,10 +40,12 @@ cat $PROJECTS_CSV_PATH | while
 
 		echo "Running refactoring analyzer"
 
-		java -Xmx650m -Xms350m -cp $REFACTORINGMINER_JAR_PATH:$JAR_PATH $CLASS $DATASET $REPO $STORAGE_PATH $URL $USER $PWD >> $8/log.txt 2>> $8/error.txt
+		java -Xmx650m -Xms350m -cp $REFACTORINGMINER_JAR_PATH:$JAR_PATH $CLASS $DATASET $REPO $STORAGE_PATH $URL $USER $PWD >> log.txt 2>> error.txt
 		if [ $? -eq 0 ]
 		then
-			echo "Zipping and sending it to the storage machine"
+			echo "Packing the java files"
+			mv log.txt $OUTPUT_PROJECT_PATH
+			mv error.txt $OUTPUT_PROJECT_PATH
 			zip -q -r $DATASET-$PROJECT.zip $OUTPUT_PROJECT_PATH/*
 			scp $DATASET-$PROJECT.zip $STORAGE_MACHINE/$DATASET-$PROJECT.zip
 			rm $DATASET-$PROJECT.zip
@@ -50,6 +53,7 @@ cat $PROJECTS_CSV_PATH | while
 
 		echo "Deleting folder"
 		rm -rf $OUTPUT_PROJECT_PATH
+		rm error.txt log.txt nohup.out
 
 		echo ""
 		echo "#####################"
