78c899777c33b4703628a994b1d41ad1faad829c
Erick Maziero <egmaziero@gmail.com>
2019-06-18 17:29:20 -0300

    precision and recall added to deep-learing

--- traditional-ml/refactoring_models_deep_learning.py
+++ traditional-ml/refactoring_models_deep_learning.py
@@ -3,10 +3,11 @@ from keras.models import Sequential
 from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import keras_metrics
 
 
 def run_deep_learning(x, columns, y, f, refactoring_name):
-
 
     x_train, x_test, y_train, y_test = train_test_split (x, y, test_size=0.2,
                                                          random_state=42)
@@ -26,7 +27,10 @@ def run_deep_learning(x, columns, y, f, refactoring_name):
     # for binary classifiers
     model.add(Dense(1, activation='sigmoid'))
     
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['accuracy',
+                           keras_metrics.precision(),
+                           keras_metrics.recall()])
 
     # for multiclass classifier
     # model.add(Dense(y.shape[1]))
@@ -40,14 +44,23 @@ def run_deep_learning(x, columns, y, f, refactoring_name):
     early_stop = EarlyStopping(monitor='val_loss', min_delta=0.001, mode='auto',
                                verbose=1, patience=50)
 
-    #fit model
+    # fit model
     model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1000,
               batch_size=128, callbacks=[early_stop])
 
-    # validation
-    scores = model.evaluate(x_test, y_test, batch_size=128)
+    # evaluate
+    y_pred = model.predict_classes(x_test)
 
-    print("Accuracy: %0.2f" % (scores[1]))
-    f.write("Accuracy: %0.2f\n" % (scores[1]))
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy: %0.2f" % (accuracy))
+    f.write("Accuracy: %0.2f\n" % (accuracy))
+    
+    precision = precision_score(y_test, y_pred)
+    print("Precision: %0.2f" % (precision))
+    f.write("Precision: %0.2f\n" % (precision))
+    
+    recall = recall_score(y_test, y_pred)
+    print("Recall: %0.2f" % (recall))
+    f.write("Recall: %0.2f\n" % (recall))
 
     return model
