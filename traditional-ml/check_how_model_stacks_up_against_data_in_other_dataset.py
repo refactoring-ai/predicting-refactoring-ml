6d0004c4072ee888020e4503b4433a4949475487
Vinicius Durelli <vinicius.durelli@gmail.com>
2019-07-16 09:56:44 -0300

    Added first version of the script to compare how a model fares when used to predicted stuff from other datasets (that haven't been used to build the said model)

---
+++ traditional-ml/check_how_model_stacks_up_against_data_in_other_dataset.py
@@ -0,0 +1,25 @@
+from joblib import load
+#from ml_utils import load_model #SEE BELOW
+#from main import datasets, models
+
+models = ['svm', 'decision-tree', 'random-forest','logistic-regression', 'svm-non-linear']
+datasets = ['', 'apache', 'github', 'fdroid']
+
+def check_model_performance(dataset, model, model_name, refactoring_name):
+    if is_not_blank(dataset) and dataset in datasets: 
+        #VALID DATASET, READY TO GO!
+        #LOAD MODEL BUILT FROM DATASET
+        model_under_eval = load_model(model, model_name, dataset, refactoring_name)
+        #LOAD REMAINING MODELS (I.E., ALL BUT THE ONE FROM THE DATASET UNDER EVALUATION)
+        models_built_from_other_datasets = [i for i in datasets if i not in ['', dataset]]
+
+#THIS METHOD SHOULD PROBABLY BE MOVED TO ml_utils
+def load_model(model, model_name, dataset, refactoring_name):
+    file_name = "results/model-" + model_name + "-" + dataset + "-" + refactoring_name.replace(" ", "") + ".joblib"
+    return load(file_name)
+
+def is_not_blank(s):
+    return bool(s and s.strip())
+
+check_model_performance('apache', 'model', 'model_name', 'class-level')
+
