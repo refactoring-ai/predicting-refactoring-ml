6472d4c9e1bf548cb108ba687c6651cc8f178ddf
Mauricio Aniche <mauricioaniche@gmail.com>
2019-07-19 16:31:34 +0200

    second attempt on cross models, this time using a previously saved scaler

--- traditional-ml/ml_utils.py
+++ traditional-ml/ml_utils.py
@@ -12,9 +12,17 @@ from sklearn.utils.multiclass import unique_labels
 from configs import BALANCE_DATASET
 
 
-def load_model(root_folder, model_name, dataset, refactoring_name):
-    file_name = root_folder + "/model-" + model_name + "-" + dataset + "-" + refactoring_name.replace(" ", "") + ".joblib"
+def load_object(root_folder, obj_descr_type, model_name, dataset, refactoring_name):
+    file_name = root_folder + "/" + obj_descr_type + "-" + model_name + "-" + dataset + "-" + refactoring_name.replace(" ", "") + ".joblib"
     return load(file_name)
+
+
+def load_model(root_folder, model_name, dataset, refactoring_name):
+    return load_object(root_folder, "model", model_name, dataset, refactoring_name)
+
+
+def load_scaler(root_folder, model_name, dataset, refactoring_name):
+    return load_object(root_folder, "scaler", model_name, dataset, refactoring_name)
 
 
 def save_model(model, model_name, dataset, refactoring_name):
