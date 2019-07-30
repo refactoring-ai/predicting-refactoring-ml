4af895b404d15834a13735875276676c9a8f8b46
dunha@cabtec.onmicrosoft.com <rafael.durelli@dcc.ufla.br>
2019-07-30 14:49:04 -0300

    alteracoes no modelo

--- PredictorAPI/api/Recommend.py
+++ PredictorAPI/api/Recommend.py
@@ -20,7 +20,128 @@ class Recommend(Resource):
     def post(self):
         logger.debug("Inside the post method of Recommend")
 
-        data = request.get_json(force=True)
-        print(data['version'])
+        all_refactoring = ["for class_level_refactoring-->", ["Extract Class",
+            "Extract Subclass",
+            "Extract Super-class",
+            "Extract Interface",
+            "Move Class",
+            "Rename Class",
+            "Move and Rename Class"],
+        "for method_level_refactoring-->", ["Extract And Move Method",
+            "Extract Method",
+            "Inline Method",
+            "Move Method",
+            "Pull Up Method",
+            "Push Down Method",
+            "Rename Method"],
+        "for variable_level_refactoring-->", ["Extract Variable",
+            "Inline Variable",
+            "Parameterize Variable",
+            "Rename Variable",
+            "Rename Parameter",
+            "Replace Variable with Attribute",
 
-        return jsonify(data)
+                           ]
+        ]
+
+        class_level_refactoring = {
+            "Extract Class": "Extract Class",
+            "Extract Subclass": "Extract Subclass",
+            "Extract Super-class": "Extract Super-class",
+            "Extract Interface": "Extract Interface",
+            "Move Class": "Move Class",
+            "Rename Class": "Rename Class",
+            "Move and Rename Class": "Move and Rename Class"
+        }
+
+        method_level_refactoring = {
+            "Extract And Move Method": "Extract And Move Method",
+            "Extract Method": "Extract Method",
+            "Inline Method": "Inline Method",
+            "Move Method": "Move Method",
+            "Pull Up Method": "Pull Up Method",
+            "Push Down Method": "Push Down Method",
+            "Rename Method": "Rename Method"
+        }
+
+        variable_level_refactoring = {
+            "Extract Variable": "Extract Variable",
+            "Inline Variable": "Inline Variable",
+            "Parameterize Variable": "Parameterize Variable",
+            "Rename Variable": "Rename Variable",
+            "Rename Parameter": "Rename Parameter",
+            "Replace Variable with Attribute": "Replace Variable with Attribute",
+        }
+
+        data = request.get_json(force=True)
+
+        if 'version' in data:
+            version = data['version']
+            if 'dataset' in data:
+                dataset = data['dataset']
+                if 'refactoring' in data:
+                    refactoring = data['refactoring']
+                    if 'model' in data:
+                        model = data['model']
+                        if (refactoring in class_level_refactoring):
+                            if 'features' in data:
+                                features = data['features']
+                                if (len(features) == 47):
+                                    # aqui deve-se chamar o modelo para predizer a refatoracao de class_level
+                                    print('tem a quantidade de features correta.')
+                                else:
+                                    message = 'Missing features for class_level_variable. ' \
+                                              'Please visit www.refactoring.io '
+                                    status_code = 400
+                                    return jsonify({'message:': message, 'status_code:': status_code})
+                            else:
+                                message = 'Missing parameters: {}'.format('features')
+                                status_code = 400
+                                return jsonify({'message:': message, 'status_code:': status_code})
+
+                        elif (refactoring in method_level_refactoring):
+                            print('Esta no metodo  refactoring')
+                            if 'features' in data:
+                                features = data['features']
+                                if (len(features) == 58):
+                                    # aqui deve-se chamar o modelo para predizer a refatoracao de class_level
+                                    print('tem a quantidade de features correta.')
+                                else:
+                                    message = 'Missing features for method_level_variable. ' \
+                                              'Please visit www.refactoring.io '
+                                    status_code = 400
+                                    return jsonify({'message:': message, 'status_code:': status_code})
+                        elif (refactoring in variable_level_refactoring):
+                            if 'features' in data:
+                                features = data['features']
+                                if (len(features) == 59):
+                                    # aqui deve-se chamar o modelo para predizer a refatoracao de class_level
+                                    print('tem a quantidade de features correta.')
+                                else:
+                                    message = 'Missing features for variable_level_variable. ' \
+                                              'Please visit www.refactoring.io '
+                                    status_code = 400
+                                    return jsonify({'message:': message, 'status_code:': status_code})
+                        else:
+                            message = 'Misspelling the refactoring name it should be: {}'.format(all_refactoring)
+                            status_code = 400
+                            return jsonify({'message:': message, 'status_code:': status_code})
+
+
+                    else:
+                        message = 'Missing parameters: {}'.format('model')
+                        status_code = 400
+                        return jsonify({'message:': message, 'status_code:': status_code})
+                else:
+                    message = 'Missing parameters: {}'.format('refactoring')
+                    status_code = 400
+                    return jsonify({'message:': message, 'status_code:': status_code})
+            else:
+                message = 'Missing parameters: {}'.format('dataset')
+                status_code = 400
+                return jsonify({'message:': message, 'status_code:': status_code})
+
+        else:
+            message = 'Missing parameters: {}'.format('version')
+            status_code = 400
+            return jsonify({'message:' : message, 'status_code:' : status_code})
