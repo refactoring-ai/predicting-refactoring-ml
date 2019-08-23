e6d93a3c4f354052920676208722509983d64c75
Mauricio Aniche <mauricioaniche@gmail.com>
2019-08-23 17:39:14 +0200

    one more shot

--- machine-learning/ml/preprocessing/sampling.py
+++ machine-learning/ml/preprocessing/sampling.py
@@ -6,22 +6,17 @@ from configs import BALANCE_DATASET_STRATEGY
 
 
 def perform_under_sampling(x, y):
-
+    rus = None
     if BALANCE_DATASET_STRATEGY == 'random':
         rus = RandomUnderSampler(random_state=42)  # 42 is a random number, just to ensure our results are reproducible
-        new_x, new_y = rus.fit_resample(x, y)
-
-    if BALANCE_DATASET_STRATEGY == 'cluster_centroids':
+    elif BALANCE_DATASET_STRATEGY == 'cluster_centroids':
         rus = ClusterCentroids(random_state=42)
-        new_x, new_y = rus.fit_resample(x, y)
-
-    if BALANCE_DATASET_STRATEGY == 'nearmiss':
+    elif BALANCE_DATASET_STRATEGY == 'nearmiss':
         rus = NearMiss(version=1)
-        new_x, new_y = rus.fit_resample(x, y)
-
     else:
         raise Exception("algorithm not found")
 
     # keeping column names
+    new_x, new_y = rus.fit_resample(x, y)
     new_x = pd.DataFrame(new_x, columns=x.columns)
     return new_x, new_y
