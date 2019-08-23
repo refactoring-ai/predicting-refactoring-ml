f8faf9692fd983c6967658562271df617d3bf574
Mauricio Aniche <mauricioaniche@gmail.com>
2019-08-23 17:33:54 +0200

    keeping column names, also in the under sampling

--- machine-learning/ml/preprocessing/sampling.py
+++ machine-learning/ml/preprocessing/sampling.py
@@ -1,4 +1,6 @@
 # more info: https://imbalanced-learn.readthedocs.io/en/stable/under_sampling.html
+import pandas as pd
+
 from imblearn.under_sampling import RandomUnderSampler, ClusterCentroids, NearMiss
 
 from configs import BALANCE_DATASET
@@ -8,14 +10,19 @@ def perform_under_sampling(x, y):
 
     if BALANCE_DATASET == 'random':
         rus = RandomUnderSampler(random_state=42)  # 42 is a random number, just to ensure our results are reproducible
-        return rus.fit_resample(x, y)
+        new_x, new_y = rus.fit_resample(x, y)
 
     if BALANCE_DATASET == 'cluster_centroids':
         rus = ClusterCentroids(random_state=42)
-        return rus.fit_resample(x, y)
+        new_x, new_y = rus.fit_resample(x, y)
 
     if BALANCE_DATASET == 'nearmiss':
         rus = NearMiss(version=1)
-        return rus.fit_resample(x, y)
+        new_x, new_y = rus.fit_resample(x, y)
 
-    raise Exception("algorithm not found")
+    else:
+        raise Exception("algorithm not found")
+
+    # keeping column names
+    new_x = pd.DataFrame(new_x, columns=x.columns)
+    return new_x, new_y
