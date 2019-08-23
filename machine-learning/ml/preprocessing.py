b6fe37bc8098176cf3e10fe7c3a7630e9699aafb
Mauricio Aniche <mauricioaniche@gmail.com>
2019-08-23 15:50:02 +0200

    using frac instead of specific sample size when testing

--- machine-learning/ml/preprocessing.py
+++ machine-learning/ml/preprocessing.py
@@ -34,8 +34,8 @@ def retrieve_labelled_instances(dataset, refactoring: LowLevelRefactoring):
 
     # if it's a test run, we reduce the sample randomly
     if TEST:
-        refactored_instances = refactored_instances.sample(n=100)
-        non_refactored_instances = non_refactored_instances.sample(n=100)
+        refactored_instances = refactored_instances.sample(frac=0.01)
+        non_refactored_instances = non_refactored_instances.frac(n=0.01)
 
     # now, combine both datasets (with both TRUE and FALSE predictions)
     assert non_refactored_instances.shape[1] == refactored_instances.shape[
