bdb7758505240de89c5f813d3adcbb494be71bf8
Mauricio Aniche <mauricioaniche@gmail.com>
2019-11-01 21:39:43 +0100

    fix imports

--- machine-learning/warm_cache.py
+++ machine-learning/warm_cache.py
@@ -1,4 +1,7 @@
-from db import refactoringdb
+from db.refactoringdb import get_non_refactored_methods, get_non_refactored_variables, get_non_refactored_classes, \
+    get_non_refactored_fields, get_refactoring_types, get_class_level_refactorings_count, get_class_level_refactorings, \
+    get_method_level_refactorings_count, get_method_level_refactorings, get_variable_level_refactorings_count, \
+    get_variable_level_refactorings, get_field_level_refactorings_count, get_field_level_refactorings
 
 datasets = ['', 'apache', 'github', 'fdroid']
 
@@ -9,40 +12,40 @@ for dataset in datasets:
     print("dataset: " + dataset)
 
     print("-- non refactored methods")
-    refactoringdb.get_non_refactored_methods(dataset)
+    get_non_refactored_methods(dataset)
     print("-- non refactored variables")
-    refactoringdb.get_non_refactored_variables(dataset)
+    get_non_refactored_variables(dataset)
     print("-- non refactored classes")
-    refactoringdb.get_non_refactored_classes(dataset)
+    get_non_refactored_classes(dataset)
     print("-- non refactored fields")
-    refactoringdb.get_non_refactored_fields(dataset)
+    get_non_refactored_fields(dataset)
 
     print("-- refactoring types")
-    refactoringdb.get_refactoring_types(dataset)
+    get_refactoring_types(dataset)
 
     print("-- class level refactoring count")
-    class_refactorings = refactoringdb.get_class_level_refactorings_count(dataset)
+    class_refactorings = get_class_level_refactorings_count(dataset)
     for refactoring_name in class_refactorings["refactoring"].values:
         print("---- " + refactoring_name)
-        refactoringdb.get_class_level_refactorings(refactoring_name, dataset)
+        get_class_level_refactorings(refactoring_name, dataset)
 
     print("-- method level refactoring count")
-    method_refactorings = refactoringdb.get_method_level_refactorings_count(dataset)
+    method_refactorings = get_method_level_refactorings_count(dataset)
     for refactoring_name in method_refactorings["refactoring"].values:
         print("---- " + refactoring_name)
-        refactoringdb.get_method_level_refactorings(refactoring_name, dataset)
+        get_method_level_refactorings(refactoring_name, dataset)
 
     print("-- variable level refactoring count")
-    variable_refactorings = refactoringdb.get_variable_level_refactorings_count(dataset)
+    variable_refactorings = get_variable_level_refactorings_count(dataset)
     for refactoring_name in variable_refactorings["refactoring"].values:
         print("---- " + refactoring_name)
-        refactoringdb.get_variable_level_refactorings(refactoring_name, dataset)
+        get_variable_level_refactorings(refactoring_name, dataset)
 
     print("-- field level refactoring count")
-    field_refactorings = refactoringdb.get_field_level_refactorings_count(dataset)
+    field_refactorings = get_field_level_refactorings_count(dataset)
     for refactoring_name in field_refactorings["refactoring"].values:
         print("---- " + refactoring_name)
-        refactoringdb.get_field_level_refactorings(refactoring_name, dataset)
+        get_field_level_refactorings(refactoring_name, dataset)
 
 
 print('end cache warmup')
