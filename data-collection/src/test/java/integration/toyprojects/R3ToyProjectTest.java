356ebf381cb7e37e0eb0d86f64320bc9f010feb4
rafaeldurelli <rafael.durelli@ufla.br>
2020-02-05 09:58:47 -0300

    new Toy test

---
+++ data-collection/src/test/java/integration/toyprojects/R3ToyProjectTest.java
@@ -0,0 +1,62 @@
+package integration.toyprojects;
+
+import integration.IntegrationBaseTest;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import refactoringml.db.No;
+import refactoringml.db.Yes;
+
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class R3ToyProjectTest extends IntegrationBaseTest {
+
+	@Override
+	protected String getRepo() {
+		return "repos/r3";
+	}
+
+	// This test helped to check if refactoring in subclasses are working.
+
+	//Push Up Attribute not working see e3e605f2d76b5e8a4d85ba0d586103834822ea40
+	//I tried to create a new class named Cat. Cat and Dog had the same field "region". Then I push it up to AnimalSuper.
+   // However the Pull Up Attribute didnt work.
+   // Refactoring Pull Up Method did work. But I identify two refactoring
+ //commit 892ffd8486daaedb5c92a548a23b87753393ce16 should show two refactoring -- Rename Class and Push Down Method
+	@Test
+	public void yes() {
+
+		List<Yes> yesList = session.createQuery("From Yes where project = :project order by refactoringDate desc")
+				.setParameter("project", project)
+				.list();
+
+		Assert.assertEquals(11, yesList.size());
+
+		assertRefactoring(yesList, "074881da657ed0a11527cb8b14bba12e4111c704", "Rename Class", 1);
+
+		assertRefactoring(yesList, "d025fed92a7253953a148f7264de28a85bc9af4e", "Rename Method", 2);
+
+		assertRefactoring(yesList, "061febd820977f2b00c4926634f09908cc5b8b08", "Rename Parameter", 2);
+
+		assertRefactoring(yesList, "4881103961cfac2afb7139c29eb10536b42bc3cd", "Move Class", 1);
+
+		assertRefactoring(yesList, "07cae36026215cefeac784c4213b2d46eb63de53", "Rename Parameter", 1);
+
+		assertRefactoring(yesList, "8fc1ff2f53a617082767b8dd0af60b978dfc6e67", "Move Class", 1);
+
+		assertRefactoring(yesList, "cf2ef5a3de59923ac000a4fe3ceeb8778229b293", "Pull Up Method", 2);
+
+		assertRefactoring(yesList, "892ffd8486daaedb5c92a548a23b87753393ce16", "Rename Class", 1);
+
+	}
+
+	@Test
+	public void no() {
+		// there are no instances of no variables, as the repo is too small
+		List<No> noList = session.createQuery("From No where project = :project")
+				.setParameter("project", project)
+				.list();
+		Assert.assertEquals(0, noList.size());
+	}
+}
