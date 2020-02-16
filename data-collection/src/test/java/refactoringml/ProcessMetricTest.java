c0cdc82154a7412c6630936a2d6af0102e887a74
Mauricio Aniche <mauricioaniche@gmail.com>
2020-02-16 10:36:29 +0100

    fix compilation error in test

--- data-collection/src/test/java/refactoringml/ProcessMetricTest.java
+++ data-collection/src/test/java/refactoringml/ProcessMetricTest.java
@@ -11,7 +11,7 @@ public class ProcessMetricTest {
 	@Test
 	public void authorOwnership() {
 		//TODO: What commit message should I use here?
-		ProcessMetric pm = new ProcessMetric("a.Java", "123", "", Calendar.getInstance());
+		ProcessMetric pm = new ProcessMetric("a.Java", "123", Calendar.getInstance());
 
 		for(int i = 0; i < 90; i++) {
 			pm.existsIn("commit","Mauricio", 10, 20);
@@ -38,7 +38,7 @@ public class ProcessMetricTest {
 		int qtyKeywords = ProcessMetric.bugKeywords.length;
 		Random rnd = new Random();
 
-		ProcessMetric pm = new ProcessMetric("a.Java", "123", "bug fix here", Calendar.getInstance());
+		ProcessMetric pm = new ProcessMetric("a.Java", "123", Calendar.getInstance());
 
 		pm.existsIn( "bug fix here","Rafael", 10, 20);
 
