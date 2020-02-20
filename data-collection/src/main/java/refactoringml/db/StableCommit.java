96383c6c45c1ff5b326d53946f01bb5ca90dd48d
Jan Gerling <jan.gerling@posteo.de>
2020-02-20 11:45:56 +0100

    Added variable commitThresholds as index to StableCommit

--- data-collection/src/main/java/refactoringml/db/StableCommit.java
+++ data-collection/src/main/java/refactoringml/db/StableCommit.java
@@ -3,8 +3,11 @@ package refactoringml.db;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "StableCommit", indexes = {@Index(columnList = "project_id"), @Index(columnList = "level"), @Index(columnList = "isTest")})
+@Table(name = "StableCommit", indexes = {@Index(columnList = "project_id"), @Index(columnList = "level"), @Index(columnList = "isTest"), @Index(columnList = "commitThreshold")})
 public class StableCommit extends Instance{
+	//The commit threshold for which this class is considered as stable,
+	private int commitThreshold;
+
 	@Deprecated // hibernate purposes
 	public StableCommit() {}
 
@@ -13,8 +16,12 @@ public class StableCommit extends Instance{
 		super(project, commitMetaData, filePath, className, classMetrics, methodMetrics, variableMetrics, fieldMetrics, level);
 	}
 
+	public int getCommitThreshold(){ return commitThreshold; }
+
 	@Override
 	public String toString() {
-		return "StableCommit{" + "id=" + id + super.toString() + '}';
+		return "StableCommit{" + "id=" + id +
+				super.toString() +
+				", commitThreshold=" + commitThreshold + '}';
 	}
 }
