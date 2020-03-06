492fc89f922a32791f60a69586274a35aa58a0be
Jan Gerling <jan.gerling@posteo.de>
2020-03-06 10:09:38 +0100

    Fix wrong refactoringminer import in run-cloud

--- data-collection/run-cloud.sh
+++ data-collection/run-cloud.sh
@@ -4,7 +4,7 @@ QUEUE_CLASS="refactoringml.RunQueue"
 IMPORT_CLASS="refactoringml.RunImport"
 
 JAR_PATH=target/refactoring-analyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-REFACTORINGMINER_JAR_PATH=lib/RefactoringMiner-20190430.jar
+REFACTORINGMINER_JAR_PATH=lib/RefactoringMiner-762e93ecafff48761eaa26e39626f43258781c4a.jar
 
 
 if [ "$TASK" = "worker" ]
