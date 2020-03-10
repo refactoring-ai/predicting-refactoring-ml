346b87e903da4c8d33e175ede9e26be2a21066b5
Jan Gerling <jan.gerling@posteo.de>
2020-03-10 11:38:28 +0100

    Improve IntegrationTest

--- data-collection/src/test/java/integration/canaryprojects/TelegramServerIntegrationTest.java
+++ data-collection/src/test/java/integration/canaryprojects/TelegramServerIntegrationTest.java
@@ -4,6 +4,7 @@ import integration.IntegrationBaseTest;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import refactoringml.db.RefactoringCommit;
 import java.util.stream.Collectors;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,8 +24,20 @@ public class TelegramServerIntegrationTest extends IntegrationBaseTest {
 
 	@Test
 	public void uniqueRefactorings(){
-		int uniqueRefactorings = getRefactoringCommits().stream().map(commit -> commit.getRefactoringSummary()).distinct().collect(Collectors.toList()).size();
-		Assert.assertEquals(729, uniqueRefactorings);
+		int uniqueRefactorings = getRefactoringCommits().stream().map(RefactoringCommit::getRefactoringSummary).distinct().collect(Collectors.toList()).size();
+		Assert.assertEquals(743, uniqueRefactorings);
+	}
+
+	@Test
+	public void stableInstances(){
+		int stableInstances15 = getStableCommits().stream().filter(instance -> instance.getCommitThreshold() == 15).collect(Collectors.toList()).size();
+		Assert.assertEquals(62, stableInstances15);
+
+		int stableInstances25 = getStableCommits().stream().filter(instance -> instance.getCommitThreshold() == 25).collect(Collectors.toList()).size();
+		Assert.assertEquals(0, stableInstances25);
+
+		int stableInstances50 = getStableCommits().stream().filter(instance -> instance.getCommitThreshold() == 50).collect(Collectors.toList()).size();
+		Assert.assertEquals(0, stableInstances50);
 	}
 
 	@Test
