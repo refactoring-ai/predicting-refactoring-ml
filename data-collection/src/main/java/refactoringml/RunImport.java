6082ee43eaa1dd3ada8e765df70f21764499b7ed
Jan Gerling <jan.gerling@posteo.de>
2020-03-11 12:20:14 +0100

    Refactor Runimport
     * add queueImportWaitTime to config
     * debug logs in RunImport

--- data-collection/src/main/java/refactoringml/RunImport.java
+++ data-collection/src/main/java/refactoringml/RunImport.java
@@ -2,16 +2,21 @@ package refactoringml;
 
 import com.rabbitmq.client.*;
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import static refactoringml.util.PropertiesUtils.getProperty;
 
 public class RunImport {
+	private static final Logger log = LogManager.getLogger(RunImport.class);
+
 	public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
 		// we gotta wait a few minutes before the queue is up...
 		// Docker stuff...
-		Thread.sleep(1000 * 60 * 1);
+		Thread.sleep(1000 * Long.parseLong(getProperty("queueImportWaitTime")));
 
 		String host = System.getenv("QUEUE_HOST");
 		String file = System.getenv("FILE_TO_IMPORT");
@@ -20,8 +25,7 @@ public class RunImport {
 		System.out.println("File: " + file);
 
 		if(file.trim().isEmpty()) {
-			System.out.println("No files to import");
-			System.exit(0);
+			throw new IllegalArgumentException("The project list is empty: " + file);
 		}
 
 		ConnectionFactory factory = new ConnectionFactory();
@@ -37,9 +41,8 @@ public class RunImport {
 			for(String line : lines) {
 				String message = line;
 				channel.basicPublish("", RunQueue.QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
-				System.out.println(" [x] Sent '" + message + "'");
+				log.debug(" [x] Queue sent '" + message + "'");
 			}
-
 		}
 	}
 }
