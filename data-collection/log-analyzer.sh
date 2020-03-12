da36d7a2fb78760db5206c691613b400ef3d9eba
Jan Gerling <jan.gerling@posteo.de>
2020-03-12 16:16:32 +0100

    Improve log quality - #95
     * improve the log-analyzer
     * minor fixes to log4j config

--- data-collection/log-analyzer.sh
+++ data-collection/log-analyzer.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-infoFile="logs/data-collection_INFO.log*"
-debugFile="logs/data-collection_DEBUG.log*"
-errorFile="logs/data-collection_ERROR.log*"
+infoFile="logs/data-collection_INFO.txt*"
+debugFile="logs/data_collection_DEBUG.txt*"
+errorFile="logs/data-collection_ERROR.txt*"
+terminalFile="docker-terminal.log"
 outFile="logs/statistics.txt"
 
 #General Statistics
 echo "------------------------General Statistics------------------------" > $outFile
-totalTime=$(grep -o '.git in [0-9]*.[0-9]*' $infoFile | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{s+=$1} END {print s}')
-totalProjects=$(grep 'refactoring- and ' $infoFile | wc -l)
+totalTime=$(grep -oh '.git in [0-9]*.[0-9]*' $infoFile | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{s+=$1} END {print s}')
+totalProjects=$(egrep -oh 'Finished mining http.+ in [0-9]+.[0-9]+ minutes' $infoFile | wc -l)
 echo "A total of ${totalProjects} projects were processed with a total App executiom time of ${totalTime} minutes." >> $outFile
 totalRefactorings=$(grep 'refactoring- and ' $infoFile | sed -e 's/Found \(.*\) refactoring-.*/\1/' | awk '{s+=$1} END {print s}')
 echo "${totalRefactorings} refactoring instances were found in total." >> $outFile
@@ -22,34 +23,59 @@ done
 echo -e "\n" >> $outFile
 
 echo "------------------------Detailed Statistics------------------------" >> $outFile
-commitCount=$(grep "Processing commit " $debugFile | wc -l)
-fastestCommitProcessingTime=$(grep "Processing commit " $debugFile | sed 's/^.*took/took/' | sed -e 's/took \(.*\) milliseconds.*/\1/' | sort -n | head -1)
-longestCommitProcessingTime=$(grep "Processing commit " $debugFile | sed 's/^.*took/took/' | sed -e 's/took \(.*\) milliseconds.*/\1/' | sort -n | tail -1)
-fastestCommitHash=$(grep "took ${fastestCommitProcessingTime} milliseconds." $debugFile | sed 's/^.*commit/commit/' | sed -e 's/commit \(.*\) took.*/\1/')
-longestCommitHash=$(grep "took ${longestCommitProcessingTime} milliseconds." $debugFile | sed 's/^.*commit/commit/' | sed -e 's/commit \(.*\) took.*/\1/')
-totalCommitProcessingTime=$(grep "Processing commit " $debugFile | sed 's/^.*took/took/' | sed -e 's/took \(.*\) milliseconds.*/\1/' | awk '{s+=$1} END {print s}')
+commitProcessStatistics=$(egrep -oh "Processing commit [a-zA-Z0-9]+ took [0-9]+ milliseconds." $debugFile)
+commitProcessingTimes=$(echo "${commitProcessStatistics}" | egrep -oh " [0-9]+ " | sort -n)
+
+commitCount=$(echo "${commitProcessStatistics}" | wc -l)
+fastestCommitProcessingTime=$(echo "${commitProcessingTimes}" | head -1)
+longestCommitProcessingTime=$(echo "${commitProcessingTimes}" | tail -1)
+fastestCommitHash=$(echo "${commitProcessStatistics}" | egrep "took${fastestCommitProcessingTime}milliseconds." | sed 's/^.*commit/commit/' | sed -e 's/commit \(.*\) took.*/\1/')
+longestCommitHash=$(echo "${commitProcessStatistics}" | egrep "took${longestCommitProcessingTime}milliseconds." | sed 's/^.*commit/commit/' | sed -e 's/commit \(.*\) took.*/\1/')
+totalCommitProcessingTime=$(echo "${commitProcessingTimes}" | awk '{s+=$1} END {print s}')
 averageCommitProcessingTime=$(($totalCommitProcessingTime / $commitCount))
 
 echo "In total ${commitCount} commits were processed in ${totalCommitProcessingTime} milliseconds, thus the average time is ${averageCommitProcessingTime} milliseconds." >> $outFile
-echo "The fastest commit was processed in ${fastestCommitProcessingTime} millisecond(s) with id: ${fastestCommitHash}" >> $outFile
-echo "The slowest commit was processed in ${longestCommitProcessingTime} milliseconds with id: ${longestCommitHash}" >> $outFile
+echo -e "The fastest commit was processed in ${fastestCommitProcessingTime} millisecond(s) with id(s):" >> $outFile
+for commit in $fastestCommitHash; do
+	echo -e "\t ${commit}" >> $outFile
+done
+echo -e "The slowest commit was processed in ${longestCommitProcessingTime} milliseconds with id(s):" >> $outFile
+for commit in $longestCommitHash; do
+	echo -e "\t ${commit}" >> $outFile
+done
 echo -e "\n" >> $outFile
 
-#
 #Exceptions
 echo "------------------------Exceptions------------------------" >> $outFile
-exceptionCount=$(egrep '^([a-zA-Z]+.)+Exception:' $errorFile | sed 's/: .*//' | grep 'Exception' |wc -l)
-uniqueExceptions=$(egrep '^([a-zA-Z]+.)+Exception:' $errorFile | sed 's/: .*//' | sort --unique | grep 'Exception')
-uniqueExceptionsCount=$(egrep '^([a-zA-Z]+.)+Exception:' $errorFile | sed 's/: .*//' | sort --unique | grep 'Exception' | wc -l)
+exceptionCount=$(egrep -oh '^([a-zA-Z]+.)+Exception:' $errorFile |wc -l)
+uniqueExceptions=$(egrep -oh '^([a-zA-Z]+.)+Exception:' $errorFile | sort --unique)
+uniqueExceptionsCount=$(echo "${uniqueExceptions}" | wc -l)
 echo "${exceptionCount} exceptions occured during runtime, ${uniqueExceptionsCount} of these exceptions were unique" >> $outFile
 
 for e in $uniqueExceptions; do
 	currentExceptionCount=$(grep "${e}" $errorFile | sed 's/: .*//' | grep 'Exception' | wc -l)
-	echo -e "\t${currentExceptionCount} ${e}s occured during runtime." >> $outFile
+	echo -e "\t${currentExceptionCount} ${e} occured during runtime." >> $outFile
+done
+echo -e "\n" >> $outFile
+
+#Errors
+echo "------------------------Errors------------------------" >> $outFile
+oomeCount=$(egrep -oh 'java.lang.OutOfMemoryError:' $terminalFile |wc -l )
+errorCount=$(($(egrep -oh '^[0-9-]+ [0-9:]+ ERROR [a-zA-Z]+:[0-9]+' $errorFile |wc -l) + $oomeCount))
+uniqueOOME=$(egrep -oh 'java.lang.OutOfMemoryError:' $terminalFile | sort --unique)
+uniqueErrorsDebug=$(egrep '^[0-9-]+ [0-9:]+ ERROR [a-zA-Z]+:[0-9]+' $errorFile | egrep -oh '[a-zA-Z]+:[0-9]+' | sort --unique)
+uniqueErrors="${uniqueOOME} ${uniqueErrorsDebug}"
+uniqueOOMECount=$(echo "${uniqueOOME}" | sort --unique | wc -l)
+uniqueErrorsCount=$(($(echo "${uniqueErrors}" | sort --unique | wc -l) + $uniqueOOMECount))
+echo "${errorCount} errors occured during runtime, ${uniqueErrorsCount} of these errors were at unique places" >> $outFile
+
+for e in $uniqueErrors; do
+	currentErrorCount=$(grep "${e}" $errorFile $terminalFile | wc -l)
+	echo -e "\t${currentErrorCount} Errors at ${e} occured during runtime." >> $outFile
 done
 echo -e "\n" >> $outFile
 
 #Individual project results
 echo "------------------------Individual Project Results------------------------" >> $outFile
-grep -A 8 'refactoring- and ' $infoFile >> $outFile
+egrep -A 10 'Finished mining http.+ in [0-9]+.[0-9]+ minutes' $infoFile >> $outFile
 echo -e "\n" >> $outFile
