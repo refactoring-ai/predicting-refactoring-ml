169862a560f190abe71a8b6a52f6ca3115581269
jan.gerling <jan.gerling@posteo.de>
2020-03-12 17:07:49 +0100

    Fix minor formatting bugs in log-analyzer

--- data-collection/log-analyzer.sh
+++ data-collection/log-analyzer.sh
@@ -3,16 +3,16 @@
 infoFile="logs/data-collection_INFO.txt*"
 debugFile="logs/data_collection_DEBUG.txt*"
 errorFile="logs/data-collection_ERROR.txt*"
-terminalFile="docker-terminal.log"
+terminalFile="logs/docker-terminal.log"
 outFile="logs/statistics.md"
 
 #General Statistics
 echo "## General Statistics" > $outFile
 totalTime=$(grep -oh '.git in [0-9]*.[0-9]*' $infoFile | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{s+=$1} END {print s}')
 totalProjects=$(egrep -oh 'Finished mining http.+ in [0-9]+.[0-9]+ minutes' $infoFile | wc -l)
-echo "A total of **${totalProjects} projects** were processed with a total App execution time of **${totalTime} minutes**." >> $outFile
+echo "A total of **${totalProjects} projects** were processed with a total App execution time of **${totalTime} minutes**.  " >> $outFile
 totalRefactorings=$(grep 'refactoring- and ' $infoFile | sed -e 's/Found \(.*\) refactoring-.*/\1/' | awk '{s+=$1} END {print s}')
-echo "**${totalRefactorings} refactoring** instances were found in total." >> $outFile
+echo "**${totalRefactorings} refactoring** instances were found in total.  " >> $outFile
 totalStableInstances=$(grep 'refactoring- and' $infoFile | sed -e 's/Found [0-9]* refactoring- and \(.*\) stable.*/\1/' | awk '{s+=$1} END {print s}')
 echo "**${totalStableInstances} stable** instances were found in total." >> $outFile
 
@@ -21,7 +21,7 @@ for ((i=15;i<=50;i+=5)); do
 	echo -e " 1. ${currentStable} stablecommit instances were found for threshold ${i}" >> $outFile
 done
 
-echo "## Detailed Statistics------------------------" >> $outFile
+echo "## Detailed Statistics" >> $outFile
 commitProcessStatistics=$(egrep -oh "Processing commit [a-zA-Z0-9]+ took [0-9]+ milliseconds." $debugFile)
 commitProcessingTimes=$(echo "${commitProcessStatistics}" | egrep -oh " [0-9]+ " | sort -n)
 
@@ -33,14 +33,14 @@ longestCommitHash=$(echo "${commitProcessStatistics}" | egrep "took${longestComm
 totalCommitProcessingTime=$(echo "${commitProcessingTimes}" | awk '{s+=$1} END {print s}')
 averageCommitProcessingTime=$(($totalCommitProcessingTime / $commitCount))
 
-echo "In **total ${commitCount} commits** were processed in **${totalCommitProcessingTime} milliseconds**, thus the average time is ${averageCommitProcessingTime} milliseconds." >> $outFile
-echo -e "The **fastest commit** was processed in **${fastestCommitProcessingTime} millisecond(s)** with id(s):" >> $outFile
+echo "In **total ${commitCount} commits** were processed in **${totalCommitProcessingTime} milliseconds**, thus the average time is ${averageCommitProcessingTime} milliseconds.  " >> $outFile
+echo -e "The **fastest commit** was processed in **${fastestCommitProcessingTime} millisecond(s)** with id(s):  " >> $outFile
 for commit in $fastestCommitHash; do
-	echo -e " 1. ${commit}" >> $outFile
+	echo -e " 1. ${commit}  " >> $outFile
 done
-echo -e "The **slowest commit** was processed in **${longestCommitProcessingTime} milliseconds** with id(s):" >> $outFile
+echo -e "The **slowest commit** was processed in **${longestCommitProcessingTime} milliseconds** with id(s):  " >> $outFile
 for commit in $longestCommitHash; do
-	echo -e " 1. ${commit}" >> $outFile
+	echo -e " * ${commit}" >> $outFile
 done
 
 #Exceptions
@@ -48,7 +48,7 @@ echo "## Exceptions" >> $outFile
 exceptionCount=$(egrep -oh '^([a-zA-Z]+.)+Exception:' $errorFile |wc -l)
 uniqueExceptions=$(egrep -oh '^([a-zA-Z]+.)+Exception:' $errorFile | sort --unique)
 uniqueExceptionsCount=$(echo "${uniqueExceptions}" | wc -l)
-echo -e "**${exceptionCount} exceptions** occurred during runtime, **${uniqueExceptionsCount}** of these exceptions were **unique**." >> $outFile
+echo -e "**${exceptionCount} exceptions** occurred during runtime, **${uniqueExceptionsCount}** of these exceptions were **unique**.  " >> $outFile
 
 for e in $uniqueExceptions; do
 	currentExceptionCount=$(grep "${e}" $errorFile | sed 's/: .*//' | grep 'Exception' | wc -l)
@@ -64,7 +64,7 @@ uniqueErrorsDebug=$(egrep '^[0-9-]+ [0-9:]+ ERROR [a-zA-Z]+:[0-9]+' $errorFile |
 uniqueErrors="${uniqueOOME} ${uniqueErrorsDebug}"
 uniqueOOMECount=$(echo "${uniqueOOME}" | sort --unique | wc -l)
 uniqueErrorsCount=$(($(echo "${uniqueErrors}" | sort --unique | wc -l) + $uniqueOOMECount))
-echo "**${errorCount} errors** occurred during runtime, **${uniqueErrorsCount}** of these errors were at **unique** places." >> $outFile
+echo "**${errorCount} errors** occurred during runtime, **${uniqueErrorsCount}** of these errors were at **unique** places.  " >> $outFile
 
 for e in $uniqueErrors; do
 	currentErrorCount=$(grep "${e}" $errorFile $terminalFile | wc -l)
@@ -73,4 +73,4 @@ done
 
 #Individual project results
 echo "## Individual Project Results" >> $outFile
-egrep -A 10 ' 1. Finished mining http.+ in [0-9]+.[0-9]+ minutes' $infoFile >> $outFile
+egrep -A 10 'Finished mining http.+ in [0-9]+.[0-9]+ minutes' $infoFile >> $outFile
