e3152dbb20344407d2818dd71c2128e8394cbf32
Jan Gerling <jan.gerling@posteo.de>
2020-03-14 01:04:36 +0100

    Fix refactoringminer import filename

--- data-collection/run-cloud.sh
+++ data-collection/run-cloud.sh
@@ -4,17 +4,16 @@ QUEUE_CLASS="refactoringml.RunQueue"
 IMPORT_CLASS="refactoringml.RunImport"
 
 JAR_PATH=target/data-collection-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-REFACTORINGMINER_JAR_PATH="lib/RefactoringMiner-[0-9a-zA-Z]*.jar"
+REFACTORINGMINER_JAR_PATH=$(find lib/ -regextype posix-extended -regex '^.*RefactoringMiner-[0-9a-zA-Z]*.jar')
 
 if [ "$TASK" = "worker" ]
 then
 
-echo "Starting the worker soon"
 RC=1
 counter=0
 #not the correct exit code, but our programes always exists with 0
 while [ $RC -ne 0 ]; do
-  if [ $counter -eq 0 ]; then echo "Starting the queue class."
+  if [ $counter -eq 0 ]; then echo "Starting the queue class..."
   else echo "Restarting the queue class for the ${counter} time."
   fi
 
@@ -25,7 +24,7 @@ done
 
 else
 
-echo "Starting the import soon"
+echo "Starting the project import..."
 java -Xmx800m -Xms350m -cp $REFACTORINGMINER_JAR_PATH:$JAR_PATH $IMPORT_CLASS
 
 fi
