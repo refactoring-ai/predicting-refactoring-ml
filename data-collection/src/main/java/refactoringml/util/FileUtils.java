0ed24b075bebd3fcb3838bf7c00d2b5bbbc49290
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-15 17:28:06 +0100

    safe delete useless methods

--- data-collection/src/main/java/refactoringml/util/FileUtils.java
+++ data-collection/src/main/java/refactoringml/util/FileUtils.java
@@ -12,23 +12,6 @@ import static refactoringml.util.FilePathUtils.*;
 
 public class FileUtils {
 
-	public static String[] getAllDirs(String path) {
-		return getAllDirs(path, null);
-	}
-
-	public static String[] getAllDirs(String path, String regex) {
-		try {
-			return Files.walk(Paths.get(path))
-					.filter(Files::isDirectory)
-					.filter(x -> !x.toAbsolutePath().toString().contains(".git"))
-					.filter(x -> (regex!=null?x.toAbsolutePath().toString().contains(regex):true))
-					.map(x -> x.toAbsolutePath().toString())
-					.toArray(String[]::new);
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	public static String[] getAllJavaFiles(String path) {
 		return getAllJavaFiles(path, null);
 	}
