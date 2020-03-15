5ce9dd26020fb788398935f338ecb85d993b40ec
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-15 18:28:16 +0100

    remove useless log4j field

--- data-collection/src/main/java/refactoringml/util/FilePathUtils.java
+++ data-collection/src/main/java/refactoringml/util/FilePathUtils.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 
 public class FilePathUtils {
-	private static final Logger log = LogManager.getLogger(FilePathUtils.class);
 
 	public static String classFromFileName (String fileName) {
 		String[] splittedFile = enforceUnixPaths(fileName).replace("\\", "/").split("/");
