e3ed3e14e8803b80c7222754285cb00c72c76521
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-15 18:50:54 +0100

    testing the store source code feature

--- data-collection/src/test/java/integration/toyprojects/R6ToyProjectTest.java
+++ data-collection/src/test/java/integration/toyprojects/R6ToyProjectTest.java
@@ -12,7 +12,11 @@ import refactoringml.util.JGitUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -100,10 +104,9 @@ public class R6ToyProjectTest extends IntegrationBaseTest {
 	}
 
 	@Test
-	void assertStoreSourceCode() throws IOException {
+	void assertStoreSourceCode() {
 
 		String[] allJavaFiles = FileUtils.getAllJavaFiles(outputDir);
-		Arrays.stream(allJavaFiles).forEach(x -> System.out.println(x));
 		Assertions.assertEquals(14, allJavaFiles.length);
 
 		List<RefactoringCommit> rcs = getRefactoringCommits();
@@ -115,10 +118,67 @@ public class R6ToyProjectTest extends IntegrationBaseTest {
 			Assertions.assertTrue(found);
 		}
 
-		// TODO: assert the content of the file
-		// Repository repo = getRepository();
-		// JGitUtils.readFileFromGit(repo, "4125d9a212381d132e47c1d53b8dbb0b0a7eb7f3", )
+		long smallestId = rcs.stream().min(Comparator.comparingLong(x -> x.getId())).get().getId();
 
+		// r1: rename variable
+		String r1Before = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + smallestId + "/before/A.java")).findFirst().get();
+		String r1After = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + smallestId + "/after/A.java")).findFirst().get();
+		String r1BeforeSourceCode = sourceCode(r1Before);
+		Assertions.assertTrue(r1BeforeSourceCode.contains("int a = 0;"));
+		String r1AfterSourceCode = sourceCode(r1After);
+		Assertions.assertTrue(r1AfterSourceCode.contains("int b = 0;"));
+
+		// r2: move source folder
+		// class A was isolated into its own class... the Utils class disappeared from A.java
+		String r2Before = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+1) + "/before/A.java")).findFirst().get();
+		String r2After = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+1) + "/after/A.java")).findFirst().get();
+		String r2BeforeSourceCode = sourceCode(r2Before);
+		Assertions.assertTrue(r2BeforeSourceCode.contains("class Utils"));
+		String r2AfterSourceCode = sourceCode(r2After);
+		Assertions.assertFalse(r2AfterSourceCode.contains("class Utils"));
+
+		// r3: move source folder
+		// connected to the previous one, but now it's the Utils class
+		String r3Before = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+2) + "/before/A.java")).findFirst().get();
+		String r3After = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+2) + "/after/Utils.java")).findFirst().get();
+		String r3BeforeSourceCode = sourceCode(r3Before);
+		Assertions.assertTrue(r3BeforeSourceCode.contains("class Utils"));
+		String r3AfterSourceCode = sourceCode(r3After);
+		Assertions.assertTrue(r3AfterSourceCode.contains("class Utils"));
+		Assertions.assertEquals(0, Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+2) + "/before/Utils.java")).count());
+
+		// r4: Extract and Move Method
+		String r4Before = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+3) + "/before/A.java")).findFirst().get();
+		String r4After = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+3) + "/after/A.java")).findFirst().get();
+		String r4After2 = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+3) + "/after/Utils.java")).findFirst().get();
+		String r4BeforeSourceCode = sourceCode(r4Before);
+		Assertions.assertTrue(r4BeforeSourceCode.contains("int c = a + b"));
+		String r4AfterSourceCode = sourceCode(r4After);
+		Assertions.assertTrue(r4AfterSourceCode.contains("new Utils().m2copy();"));
+		String r4AfterSourceCode2 = sourceCode(r4After2);
+		Assertions.assertTrue(r4AfterSourceCode2.contains("public void m2copy"));
+		Assertions.assertEquals(0, Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+3) + "/before/Utils.java")).count());
+
+		// r5: move and inline method
+		String r5Before = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+4) + "/before/A.java")).findFirst().get();
+		String r5Before2 = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+4) + "/before/Utils.java")).findFirst().get();
+		String r5After = Arrays.stream(allJavaFiles).filter(x -> x.contains("/" + (smallestId+4) + "/after/A.java")).findFirst().get();
+
+		String r5BeforeSourceCode = sourceCode(r5Before);
+		Assertions.assertTrue(r5BeforeSourceCode.contains("new Utils().m2copy();"));
+		String r5BeforeSourceCode2 = sourceCode(r5Before2);
+		Assertions.assertTrue(r5BeforeSourceCode2.contains("void m2copy()"));
+		String r5AfterSourceCode = sourceCode(r5After);
+		Assertions.assertTrue(r5AfterSourceCode.contains("int c = a + b"));
+
+	}
+
+	private String sourceCode(String file) {
+		try {
+			return new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private Repository getRepository() throws IOException {
