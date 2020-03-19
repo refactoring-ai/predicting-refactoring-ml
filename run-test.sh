aaedbe2ebe8ce74f85128b93ef688ed4fd6d582a
Jan Gerling <jan.gerling@posteo.de>
2020-03-19 14:33:41 +0100

    Stress test config 06

--- run-test.sh
+++ run-test.sh
@@ -8,5 +8,4 @@ terminalLog="docker-terminal.log"
 cd data-collection
 mkdir -p $logDir
 mvn -DskipTests clean compile package
-docker-compose up --scale worker=3 |&tee "${logDir}${terminalLog}"
-
+docker-compose up --scale worker=4 |&tee "${logDir}${terminalLog}"
