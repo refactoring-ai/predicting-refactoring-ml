b8a9259cc6bf2fc32ba3ac696745d0bde0bafe4f
Jan Gerling <jan.gerling@posteo.de>
2020-03-23 15:52:48 +0100

    ProcessMetrics test for toyproject 07

--- data-collection/src/test/java/integration/toyprojects/R7ToyProjectTest.java
+++ data-collection/src/test/java/integration/toyprojects/R7ToyProjectTest.java
@@ -6,6 +6,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import refactoringml.db.ProcessMetrics;
 import refactoringml.db.RefactoringCommit;
 import refactoringml.util.FileUtils;
 
@@ -36,5 +37,20 @@ public class R7ToyProjectTest extends IntegrationBaseTest {
 		Assertions.assertEquals(6, refactorings.size());
 
 		Assertions.assertTrue(refactorings.stream().anyMatch(x -> x.getRefactoring().equals("Extract Interface")));
+	}
+
+	@Test
+	void processMetrics(){
+		String moveRefactoring = "dce3865b05fe0b6e1db8e23f17dec498018d3f2f";
+		RefactoringCommit moveRefactoringCommit = (RefactoringCommit) filterCommit(getRefactoringCommits(), moveRefactoring).get(0);
+		assertProcessMetrics(moveRefactoringCommit, ProcessMetrics.toString(1, 14, 0, 1, 0, 1, 1.0, 0, 0));
+
+		String moveRefactoring02 = "9cc7c77cfd38210eb44a67adda0545c0b6655017";
+		RefactoringCommit moveRefactoringCommit02 = (RefactoringCommit) filterCommit(getRefactoringCommits(), moveRefactoring02).get(0);
+		assertProcessMetrics(moveRefactoringCommit02, ProcessMetrics.toString(2, 28, 14, 1, 0, 1, 1.0, 0, 1));
+
+		String lastRefactoring = "a0f7fbbfb859a23027705d2fdb12291795752736";
+		RefactoringCommit lastRefactoringCommit = (RefactoringCommit) filterCommit(getRefactoringCommits(), lastRefactoring).get(0);
+		assertProcessMetrics(lastRefactoringCommit, ProcessMetrics.toString(4, 34, 17, 1, 0, 1, 1.0, 0, 5));
 	}
 }
